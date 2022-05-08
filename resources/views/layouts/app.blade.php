973d239f32a9243000a632e48bbdac44268c2738
Thibaut Meslin <71119942+ThibautFaitDesTartiflettes@users.noreply.github.com>
2022-05-08 19:08:11 +0200

    install TailwindCSS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portfolio - Thibaut Meslin</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     @include('partials.navbar')
     @yield('content')
+    <script src="{{ asset('css/app.css') }}"></script>
 </body>
 </html>
