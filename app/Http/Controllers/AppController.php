0fbae3342db8a75e839fce9f9a5f5e8dd9d4e759
Thibaut Meslin <71119942+ThibautFaitDesTartiflettes@users.noreply.github.com>
2022-05-25 10:57:09 +0200

    resolve errors

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-class AppController extends Controllers{
-    public function show(){
+
+class AppController extends Controller{
+    public function index(){
         return view('home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AppController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [AppController::class, 'show'])->name('home');
+Route::get('/', [AppController::class, 'index'])->name('home');
