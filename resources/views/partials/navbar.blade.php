290d8bc71d375c4a9c6fe12572c58e39b4a2067a
Thibaut Meslin <71119942+ThibautFaitDesTartiflettes@users.noreply.github.com>
2022-05-26 22:04:53 +0200

    Navbar

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,13 @@
+<header class="text-white bg-stone-800 body-font">
+    <div class="container mx-auto flex flex-wrap p-4 flex-col md:flex-row items-center">
+        <a class="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+            <span class="ml-3 text-xl">Thibaut Meslin</span>
+        </a>
+        <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center cursor-pointer">
+            <a class="mr-5 hover:text-white">About me</a>
+            <a class="mr-5 hover:text-white">Projects</a>
+            <a class="mr-5 hover:text-white">Internships & Jobs</a>
+            <a class="mr-5 hover:text-white">Contact me</a>
+        </nav>
+    </div>
+</header>
