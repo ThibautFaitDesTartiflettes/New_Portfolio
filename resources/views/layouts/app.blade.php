47b1480bae5e07247af7feed397652117e42552a
Thibaut Meslin <71119942+ThibautFaitDesTartiflettes@users.noreply.github.com>
2022-06-07 20:07:44 +0200

    Add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portfolio - Thibaut Meslin</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
 </head>
 <body>
     @include('partials.navbar')
